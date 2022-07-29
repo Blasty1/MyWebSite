19ba88a3fc026f085618005f4dd9a172ea6e853e
Bruno <ichigoplayer18@gmail.com>
2022-07-29 10:41:52 +0200

    Heroku config

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href=" {{ mix('css/app.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+  <link rel="stylesheet" href="css/app.css">
+  <link rel="stylesheet" href="/css/home.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/tiny-slider.css">
   <link rel="stylesheet" href="/css/slider.css">
 
@@ -215,8 +215,8 @@
   </div>
 
 
-  <script src="{{ mix('js/app.js') }}"></script>
-  <script src="{{ asset('js/home.js')}}"></script>
+  <script src="js/app.js"></script>
+  <script src="js/home.js"></script>
 
 
 </body>
